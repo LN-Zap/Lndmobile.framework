16497f46ba5ded608efcd1adfe81b7a7de2e8a77
raph <maraph@users.noreply.github.com>
2019-10-24 15:03:13 +0200

    Add Android

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Lndmobile.framework [![Build Status](https://travis-ci.com/LN-Zap/Lndmobile.framework.svg?branch=master)](https://travis-ci.com/LN-Zap/Lndmobile.framework)
 
-compiled lnd for iOS 🎉
+compiled lnd for iOS and Android 🎉
--- build_iOS.sh
+++ build_iOS.sh
@@ -4,7 +4,8 @@ cd "`dirname \"$0\"`"
 
 export REPOROOT=$(pwd)
 export GOPATH=$REPOROOT/.build
-export PATH=$GOPATH/bin:$PATH
+export GOBIN=$GOPATH/bin
+export PATH=$GOBIN:$PATH
 
 set -v
 
@@ -53,7 +54,7 @@ git checkout v0.8.0-beta
 git apply $REPOROOT/patches/user_agent_name.patch
 git apply $REPOROOT/patches/user_agent_version.patch
 
-make tags="experimental" IOS_BUILD_DIR=$REPOROOT ios
+make GOPATH=$GOPATH IOS_BUILD_DIR=$REPOROOT ios
 
 cd $REPOROOT
 zip --symlinks -r Lndmobile.framework.zip Lndmobile.framework
@@ -69,5 +70,4 @@ protoc -I/usr/local/include -I.\
        --zap_out=$OUT \
        --swiftgrpc_out=Sync=false,Server=false:$OUT \
        $GOPATH/src/github.com/lightningnetwork/lnd/lnrpc/rpc.proto
-zip -j -r swift-generated.zip generated
-
+zip -j -r Lndmobile.swift-generated.zip generated
